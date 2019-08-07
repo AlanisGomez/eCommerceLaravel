8f307979d5fc3b214ef30a64c00ac7602d31d2be
gisellegalvan05 <giseg5@hotmail.com>
2019-08-06 22:03:58 -0300

    Fixes carrito

--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -6,36 +6,38 @@
 
 @section('content')
     @if(Session::has('cart'))
-        <div class="row">
-            <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
-                <ul class="list-group">
-                    @foreach($productos as $producto)
-                            <li class="list-group-item">
-                                <span class="badge">{{ $producto['qty'] }}</span>
-                                <strong>{{ $producto['item']['id'] }}</strong>
-                                <span class="label label-success">{{ $producto['price'] }}</span>
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-primary btn-xs dropdown-toogle" data-toggle="dropdown">Action <span class="caret"></span></button>
-                                    <ul class="dropdown-menu">
-                                        <li><a href="{{ route('product.reduceByOne', ['id' => $producto['item']['id']]) }}">Reduce by 1</a></li>
-                                        <li><a href="{{ route('product.remove', ['id' => $producto['item']['id']]) }}">Reduce All</a></li>
-                                    </ul>
+        <div class="contentCart">
+          <div class="row justifyCenter">
+              <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+                  <ul class="list-group">
+                      @foreach($productos as $producto)
+                              <li class="list-group-item">
+                                <div class="col-3">
+                                  <span class="badge">{{ $producto['qty'] }}</span>
+                                  <strong>{{ $producto['item']['nombre'] }}</strong>
                                 </div>
-                            </li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
-                <strong>Total: {{ $totalPrice }}</strong>
-            </div>
-        </div>
-        <hr>
-        <div class="row">
-            <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
-                <a href="{{ route('checkout') }}" type="button" class="btn btn-success">Checkout</a>
-            </div>
+                                <div class="col-3">
+                                  <span class="label label-success">${{ $producto['item']['precio'] }}</span>
+                                </div>
+                                <div class="btn-group col-3">
+                                    <a class="btn btn-primary btn-xs" href="{{ route('product.reduceByOne', ['id' => $producto['item']['id']]) }}">Quitar del carrito</a>                                      
+                                  </div>
+                              </li>
+                      @endforeach
+                  </ul>
+              </div>
+          </div>
+          <div class="row justifyCenter">
+              <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+                  <strong>Total: {{ $totalPrice }}</strong>
+              </div>
+          </div>
+          <hr>
+          <div class="row justifyCenter">
+              <div class="col-sm-6 col-md-6 col-md-offset-3 col-sm-offset-3">
+                  <a href="{{ route('checkout') }}" type="button" class="btn btn-success">Comprar</a>
+              </div>
+          </div>
         </div>
     @else
         <div class="row">
